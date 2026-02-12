d1313e01fd7ca9a9840c3707d1c9f5bc7d94e3e9
smazumder <smazumder@qnx.com>
2026-02-12 18:24:49 -0500

    udpating repo

--- docs/codelabs-list.js
+++ docs/codelabs-list.js
@@ -1,20 +1,5 @@
 window.CODELABS = [
   {
-    "title": "Codelabs to create Codelabs",
-    "duration": "9 min",
-    "href": "codelab-for-codelab/index.html",
-    "id": "codelab-for-codelab",
-    "authors": "Your Team",
-    "updated": "2026-02-10T16:42:30-05:00",
-    "category": [
-      "codelabs",
-      "setup"
-    ],
-    "tags": [
-      "beginner"
-    ]
-  },
-  {
     "title": "How to Create Your Own Codelab and Contribute to QNX",
     "duration": "19 min",
     "href": "codelab-to-create-codelab/index.html",
